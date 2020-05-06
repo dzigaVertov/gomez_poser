76b55593e543121087be7860ba01548862911fce
marcelo <gomezmarcelod@gmail.com>
2020-05-05 21:39:13 -0300

    fixed the context in order to call de fit_curve operator

--- gomez_poser_ui.py
+++ gomez_poser_ui.py
@@ -47,11 +47,16 @@ def add_auxiliary_meshes():
     Crear objetos para reemplazar la apariencia de ctrl bones
     """
     initialized = C.window_manager.gopo_prop_group.initialized
+    if initialized:
+        return
+    
     # Crear collection para guardar meshes auxiliares
     aux_col = bpy.data.collections.new('auxiliary_meshes')
     C.scene.collection.children.link(aux_col)
     aux_col.hide_viewport = True
     aux_col.hide_render = True
+
+    # ESFERA
     # Crear objeto y mesh vacíos
     control_sphere_mesh = bpy.data.meshes.new('ctrl_sphere')
     ctrl_sphere_ob = bpy.data.objects.new('ctrl_sphere', control_sphere_mesh)
@@ -67,6 +72,23 @@ def add_auxiliary_meshes():
     bm.free()
 
     ctrl_sphere_ob.display_type = 'WIRE'
+
+    # CONO
+    # Crear objeto y mesh vacíos
+    control_cone_mesh = bpy.data.meshes.new('ctrl_cone')
+    ctrl_cone_ob = bpy.data.objects.new('ctrl_cone', control_cone_mesh)
+    # Linkear objeto a collection, hacer activo
+    aux_col.objects.link(ctrl_cone_ob)
+    ctrl_cone_ob.select_set(True)
+    C.view_layer.objects.active = ctrl_cone_ob
+
+    # Crear geometría
+    bm = bmesh.new()
+    bmesh.ops.create_cone(bm, cap_ends=False, cap_tris=True, segments= 8, diameter1=0.25, depth=0.25)
+    bm.to_mesh(control_cone_mesh)
+    bm.free()
+
+    ctrl_cone_ob.display_type = 'WIRE'
 
     initialized = True
 
@@ -159,7 +181,7 @@ def get_bones_positions(stroke):
     huesos a lo largo del stroke
     """
     h_coefs = C.window_manager.fitted_curve_coefs
-    bones_positions = [(i.ctrl_point1, i.ctrl_point1) for i in h_coefs]
+    bones_positions = [(i.ctrl_point, j.ctrl_point) for i,j in zip(h_coefs[:-1],h_coefs[1:])]
     return bones_positions
 
 
@@ -178,8 +200,10 @@ def add_deform_bones(armature, pos):
     ed_bones = armature.data.edit_bones
 
     for i,pos in enumerate(pos):
+        print('creating a boney bone')
         head, tail = pos
         name = 'boney_' + str(i)
+        print('bone_name: ', name)
         ed_bones.new(name)
 
         ed_bones[name].head = head
@@ -205,11 +229,11 @@ def add_handles(armature, name, i):
     """
     Sets handle bones as bezier handles for the deform bone
     """
-    bones = armature.bones
+    bones = armature.data.bones
     def_bone = bones['boney_' + str(i)]
-    def_bone.bbone_custom_handle_start = name + '_left_' + str(i)
+    def_bone.bbone_custom_handle_start = bones[name + '_left_' + str(i)]
     def_bone.bbone_handle_type_start = 'ABSOLUTE'
-    def_bone.bbone_custom_handle_end = name + '_right_' + str(i)
+    def_bone.bbone_custom_handle_end = bones[name + '_right_' + str(i)]
     def_bone.bbone_handle_type_end = 'ABSOLUTE'
 
 def add_copy_location(armature, name, i):
@@ -245,7 +269,7 @@ def add_control_bones(armature, pos):
     Adds custom shapes - Puts control bones in first layer.
     """
     h_coefs = C.window_manager.fitted_curve_coefs
-    handles = [(i.h_left, i.h_right) for i in h_coefs ]
+    handles = [(i.handle_l, i.handle_r) for i in h_coefs ]
     
     armature.select_set(True)
     C.view_layer.objects.active = armature
@@ -254,15 +278,15 @@ def add_control_bones(armature, pos):
 
     for i, p in enumerate(pos):
         name = 'ctrl_stroke_' + str(i)
-        ctrl, _ = pos
+        ctrl, _ = p
         ed_bones.new(name)
-        ed_bones[name].head = ctrl
-        ed_bones[name].tail = ctrl + Vector((0.0, 0.0, 1.0))
+        ed_bones[name].head = Vector(ctrl)
+        ed_bones[name].tail = Vector(ctrl) + Vector((0.0, 0.0, 1.0))
         ed_bones[name].use_deform = False
 
         
-    for i, h in handles:
-        h_left, h_right = h
+    for i, h in enumerate(handles):
+        h_left, h_right = map(Vector, h)
         name_left = 'handle_left_' + str(i)
         name_right = 'handle_right_' + str(i)
         ed_bones.new(name_left)
@@ -292,6 +316,13 @@ def add_control_bones(armature, pos):
         if bone.name.startswith('ctrl'):
             bone.custom_shape = D.objects['ctrl_sphere']
             bone.custom_shape_scale = 0.5
+            armature.data.bones[bone.name].show_wire = True
+            armature.data.bones[bone.name].layers[0] = True
+            armature.data.bones[bone.name].layers[-1] = False
+
+        if bone.name.startswith('handle'):
+            bone.custom_shape = D.objects['ctrl_cone']
+            bone.custom_shape_scale = 0.25
             armature.data.bones[bone.name].show_wire = True
             armature.data.bones[bone.name].layers[0] = True
             armature.data.bones[bone.name].layers[-1] = False
@@ -395,21 +426,24 @@ def fit_and_add_bones(armature, gp_ob):
     # fit the curve
     error = C.window_manager.gopo_prop_group.error_threshold
     bpy.ops.gpencil.fit_curve(error_threshold=error, target='ARMATURE')
-
+    obarm = armature
+    bones = obarm.data.bones
+    
+    print(len(bones))
     pos = get_bones_positions(stroke)
-
+    print(len(bones))
     add_deform_bones(armature, pos)
-
+    print(len(bones))
     add_control_bones(armature, pos)
-
+    print(len(bones))
     add_armature(gp_ob, stroke, armature)
-
+    print(len(bones))
     add_vertex_groups(gp_ob, armature)
-
+    print(len(bones))
     add_weights(gp_ob, stroke)
-
+    print(len(bones))
     prepare_interface(armature)
-
+    print(len(bones))
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
