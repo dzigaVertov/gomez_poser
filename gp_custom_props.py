6d45da4e47648afb7bfc1ebf4a5b0f1532673790
marcelo <gomezmarcelod@gmail.com>
2020-06-20 21:48:23 -0300

    added custom props to bones to replace the name centered system

--- gp_custom_props.py
+++ gp_custom_props.py
@@ -87,6 +87,18 @@ def register():
                                                    description='id of linked stroke',
                                                    default=0)
 
+    bpy.types.Bone.bone_type = StringProperty(name='bone_type',
+                                              description='Describes the use given to the bone by the addon, either DEFORM, CTRL, HANDLE_LEFT, HANDLE_RIGHT or ROOT',
+                                              default='')
+    bpy.types.EditBone.bone_type = StringProperty(name='bone_type',
+                                                  description='Describes the use given to the bone by the addon, either DEFORM, CTRL, HANDLE_LEFT, HANDLE_RIGHT or ROOT',
+                                                  default='')
+    bpy.types.Bone.bone_order = IntProperty(name='bone_order',
+                                            description='Place that the bone occupies in a chain, starting from 0',
+                                            default=-1)
+    bpy.types.Bone.bone_order = IntProperty(name='bone_order',
+                                            description='Place that the bone occupies in a chain, starting from 0',
+                                            default=-1)
 
 def unregister():
     bpy.utils.unregister_class(FittedBone)
@@ -96,3 +108,7 @@ def unregister():
     del bpy.types.WindowManager.fitted_bones
     del bpy.types.Bone.rigged_stroke
     del bpy.types.EditBone.rigged_stroke
+    del bpy.types.Bone.bone_type
+    del bpy.types.EditBone.bone_type
+    del bpy.types.Bone.bone_order
+    del bpy.types.EditBone.bone_order
